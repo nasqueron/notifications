c25591b11f269f415a4076a0023fae1888abea35
Sébastien Santoro <dereckson@espace-win.org>
2015-11-22 04:04:15 +0000

    Unit test for placeholder
    This is a follow-up of 77aa28f29 to adapt the example test
    to our placeholder.

--- tests/PlaceholderTest.php
+++ tests/PlaceholderTest.php
@@ -4,16 +4,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class PlaceholderTest extends TestCase
 {
     /**
-     * A basic functional test example.
+     * Placeholder homepage works.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testPlaceholder()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+             ->see('Notifications center');
     }
 }
