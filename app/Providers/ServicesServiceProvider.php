343dd7c666c27f1b8ced4fca7d1c07aa9f4cb1bd
Sébastien Santoro <dereckson@espace-win.org>
2016-01-09 04:50:54 +0000

    Provide a void Services instance as fallback
    Summary:
    The configuration key security.gate.credentials allows to define
    a credentials.json file with the list of services credentials.
    
    When the file doesn't exist, instead of deserializing this file,
    we provide an empty Services instance.
    
    This fixes a regression introduced at D180.
    
    Test Plan: `phpunit`
    
    Reviewers: dereckson
    
    Differential Revision: http://devcentral.nasqueron.org/D196

--- app/Providers/ServicesServiceProvider.php
+++ app/Providers/ServicesServiceProvider.php
@@ -11,13 +11,15 @@ class ServicesServiceProvider extends ServiceProvider {
      * Registers the application services.
      *
      * @return void
-     *
-     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
      */
     public function register() {
         $this->app->singleton('services', function ($app) {
             $path = config('services.gate.credentials');
-            return Services::loadFromJson($path);
+            if ($app->make('filesystem')->has($path)) {
+                return Services::loadFromJson($path);
+            }
+
+            return new Services;
         });
     }
 }
