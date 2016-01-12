f2d7caa4785c3f451158a8f552236be8c518633f
Sébastien Santoro <dereckson@espace-win.org>
2016-01-12 16:30:07 +0000

    Tests for Services
    Test Plan: phpunit through arc unit and Jenkins + coverage check
    
    Reviewers: dereckson
    
    Subscribers: alken-orin
    
    Differential Revision: http://devcentral.nasqueron.org/D239

---
+++ tests/Services/ServicesTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Nasqueron\Notifications\Tests\Services;
+
+use Nasqueron\Notifications\Services\Services;
+use Nasqueron\Notifications\Tests\TestCase;
+
+class ServicesTest extends TestCase {
+
+    private $services;
+
+    public function setUp () {
+        parent::setUp();
+
+        $this->services = Services::loadFromJson('credentials.json');
+    }
+
+    public function testGet () {
+        $actualServices = $this->services->get();
+
+        $this->assertGreaterThan(0, $actualServices);
+
+        $this->assertEquals(
+            $this->services->services, // This is public, so testable
+            $actualServices
+        );
+
+        foreach ($actualServices as $service) {
+            $this->assertInstanceOf(
+                'Nasqueron\Notifications\Services\Service',
+                $service
+            );
+        }
+    }
+
+    public function testGetForGate () {
+        $actualServices = $this->services->getForGate('GitHub');
+        $this->assertGreaterThan(0, $actualServices);
+        foreach ($actualServices as $service) {
+            $this->assertInstanceOf(
+                'Nasqueron\Notifications\Services\Service',
+                $service
+            );
+            $this->assertEquals('GitHub', $service->gate);
+        }
+    }
+
+    public function testFindServiceByDoor () {
+        // Search gives a result
+
+        $service = $this->services->findServiceByDoor('GitHub', 'Acme');
+        $this->assertInstanceOf(
+                'Nasqueron\Notifications\Services\Service',
+                $service
+        );
+        $this->assertEquals('GitHub', $service->gate);
+        $this->assertEquals('Acme', $service->door);
+
+        // Search doesn't give any result
+
+        $service = $this->services->findServiceByDoor('GitHub', 'Quux');
+        $this->assertNull($service);
+    }
+
+    public function testFindServiceByProperty () {
+        // Search gives a result
+
+        $service = $this->services->findServiceByProperty(
+            'Phabricator',
+            'instance',
+            'https://phabricator.acme.tld'
+        );
+        $this->assertInstanceOf(
+                'Nasqueron\Notifications\Services\Service',
+                $service
+        );
+        $this->assertEquals('Phabricator', $service->gate);
+        $this->assertEquals('Acme', $service->door);
+
+        // Search doesn't give any result
+
+        $service = $this->services->findServiceByProperty(
+            'Phabricator',
+            'instance',
+            'https://notfound.acme.tld'
+        );
+        $this->assertNull($service);
+    }
+
+}
