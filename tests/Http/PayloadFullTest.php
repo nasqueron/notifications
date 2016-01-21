087c8295707602596bd9cafb42390af04e27bfdd
Sébastien Santoro <dereckson@espace-win.org>
2016-01-21 01:47:14 +0000

    Integration test: 404 for not existing Phabricator door
    Summary:
    Phabricator doors must be declared in credentials.json. We ensure
    if a non existing door is used, a 404 error is returned.
    
    This will achieve full coverage of the PhabricatorGateController.
    
    Test Plan: Run tests
    
    Reviewers: dereckson
    
    Subscribers: alken-orin
    
    Differential Revision: http://devcentral.nasqueron.org/D249

--- tests/Http/PayloadFullTest.php
+++ tests/Http/PayloadFullTest.php
@@ -77,23 +77,25 @@ class PayloadFullTest extends TestCase {
      * Tests a Phabricator gate payload.
      */
     public function testPhabricatorPayload () {
-        $this->post(
-            '/gate/Phabricator/Acme',
-            [
-                'storyID' => 3849,
-                'storyType' => 'PhabricatorApplicationTransactionFeedStory',
-                'storyData[objectPHID]' => 'PHID-TASK-l34fw5wievp6n6rnvpuk',
-                'storyData[transactionPHIDs][PHID-XACT-TASK-by2g3dtlfq3l2wc]' => 'PHID-XACT-TASK-by2g3dtlfq3l2wc',
-                'storyAuthorPHID' => 'PHID-USER-fnetlprx7zdotfm2hdrz',
-                'storyText' => 'quux moved T123: Lorem ipsum dolor to Backlog on the Foo workboard.',
-                'epoch' => 1450654419,
-            ]
-        )->seeJson([
+        $data = [
+            'storyID' => 3849,
+            'storyType' => 'PhabricatorApplicationTransactionFeedStory',
+            'storyData[objectPHID]' => 'PHID-TASK-l34fw5wievp6n6rnvpuk',
+            'storyData[transactionPHIDs][PHID-XACT-TASK-by2g3dtlfq3l2wc]' => 'PHID-XACT-TASK-by2g3dtlfq3l2wc',
+            'storyAuthorPHID' => 'PHID-USER-fnetlprx7zdotfm2hdrz',
+            'storyText' => 'quux moved T123: Lorem ipsum dolor to Backlog on the Foo workboard.',
+            'epoch' => 1450654419,
+        ];
+
+        $this->post('/gate/Phabricator/Acme', $data)->seeJson([
             'gate' => 'Phabricator',
             'door' => 'Acme',
             'action' => 'AMQPAction'
         ]);
         $this->assertResponseOk();
+
+        $this->post('/gate/Phabricator/NotExistingDoor', $data);
+        $this->assertResponseStatus(404);
     }
 
     /**
