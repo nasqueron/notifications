57a8f17463ab1a57708d4f2722a4c97bccc9122f
Sébastien Santoro <dereckson@espace-win.org>
2016-03-02 03:49:56 +0000

    Sort facades in app config
    Test Plan: no op
    
    Reviewers: dereckson
    
    Subscribers: alken-orin
    
    Differential Revision: http://devcentral.nasqueron.org/D312

--- config/app.php
+++ config/app.php
@@ -208,12 +208,13 @@ return [
     */
 
     'aliases' => [
-
+        /*
+         * Laravel Framework aliases...
+         */
         'App'       => Illuminate\Support\Facades\App::class,
         'Artisan'   => Illuminate\Support\Facades\Artisan::class,
         'Auth'      => Illuminate\Support\Facades\Auth::class,
         'Blade'     => Illuminate\Support\Facades\Blade::class,
-        'Broker'    => Nasqueron\Notifications\Facades\Broker::class,
         'Bus'       => Illuminate\Support\Facades\Bus::class,
         'Cache'     => Illuminate\Support\Facades\Cache::class,
         'Config'    => Illuminate\Support\Facades\Config::class,
@@ -232,20 +233,25 @@ return [
         'Mail'      => Illuminate\Support\Facades\Mail::class,
         'Password'  => Illuminate\Support\Facades\Password::class,
         'Queue'     => Illuminate\Support\Facades\Queue::class,
-        'Raven'     => Nasqueron\Notifications\Facades\Raven::class,
         'Redirect'  => Illuminate\Support\Facades\Redirect::class,
         'Redis'     => Illuminate\Support\Facades\Redis::class,
-        'Report'    => Nasqueron\Notifications\Facades\Report::class,
         'Request'   => Illuminate\Support\Facades\Request::class,
         'Response'  => Illuminate\Support\Facades\Response::class,
         'Route'     => Illuminate\Support\Facades\Route::class,
         'Schema'    => Illuminate\Support\Facades\Schema::class,
         'Session'   => Illuminate\Support\Facades\Session::class,
-        'Services'  => Nasqueron\Notifications\Facades\Services::class,
         'Storage'   => Illuminate\Support\Facades\Storage::class,
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        /*
+         * App aliases...
+         */
+        'Broker'    => Nasqueron\Notifications\Facades\Broker::class,
+        'Raven'     => Nasqueron\Notifications\Facades\Raven::class,
+        'Report'    => Nasqueron\Notifications\Facades\Report::class,
+        'Services'  => Nasqueron\Notifications\Facades\Services::class,
 
     ],
 
