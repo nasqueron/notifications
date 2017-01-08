691289cb6db06bef5e893ba05f147e0dedd4dfbc
Sébastien Santoro <dereckson@espace-win.org>
2017-01-08 03:02:51 +0000

    Remove GitHub to AMQP mirror feature
    Summary:
    Before the notifications system, we introduced a direct GitHub to AMQP broker
    gateway at 7e5bd3c8b.
    
    This system helped to implement the broker aspects, but was then superseded
    by our standard notifications format.
    
    As we strive to simplify the events propagation to ease to add more services,
    and as nobody seems to use it, we remove this legacy feature.
    
    Test Plan: Run tests. No unit test covered the feature.
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Differential Revision: https://devcentral.nasqueron.org/D808

--- app/Listeners/AMQPEventListener.php
+++ app/Listeners/AMQPEventListener.php
@@ -2,73 +2,13 @@
 
 namespace Nasqueron\Notifications\Listeners;
 
-use Nasqueron\Notifications\Events\GitHubPayloadEvent;
 use Nasqueron\Notifications\Events\NotificationEvent;
-use Nasqueron\Notifications\Analyzers\GitHub\GitHubPayloadAnalyzer;
 use Nasqueron\Notifications\Jobs\SendMessageToBroker;
 use Nasqueron\Notifications\Notifications\Notification;
 
 use Config;
 
 class AMQPEventListener {
-    ///
-    /// GitHub events
-    ///
-
-    /**
-     * Gets routing key, to allow consumers to select the topic they subscribe to.
-     *
-     * @param GitHubPayloadEvent $event the payload event
-     */
-    protected static function getGitHubEventRoutingKey (GitHubPayloadEvent $event) : string {
-       $key = [
-           strtolower($event->door),
-           self::getGroup($event),
-           $event->event
-       ];
-       return implode('.', $key);
-    }
-
-    protected static function getAnalyzer (GitHubPayloadEvent $event) : GitHubPayloadAnalyzer {
-        return new GitHubPayloadAnalyzer(
-            $event->door,
-            $event->event,
-            $event->payload
-        );
-    }
-
-    /**
-     * Gets the group for a specific payload
-     *
-     * @return string the group, central part of the routing key
-     */
-    protected static function getGroup (GitHubPayloadEvent $event) : string {
-        $analyzer = self::getAnalyzer($event);
-        return $analyzer->getGroup();
-    }
-
-    /**
-     * Handles a GitHub payload event.
-     *
-     * @param GitHubPayloadEvent $event
-     */
-    public function onGitHubPayload(GitHubPayloadEvent $event) : void {
-        $this->sendRawPayload($event);
-    }
-
-    /**
-     * This is our gateway GitHub Webhooks -> Broker
-     *
-     * @param GitHubPayloadEvent $event
-     */
-    protected function sendRawPayload(GitHubPayloadEvent $event) : void {
-        $target = Config::get('broker.targets.github_events');
-        $routingKey = static::getGitHubEventRoutingKey($event);
-        $message = json_encode($event->payload);
-
-        $job = new SendMessageToBroker($target, $routingKey, $message);
-        $job->handle();
-    }
 
     ///
     /// Notifications
@@ -126,10 +66,6 @@ class AMQPEventListener {
      */
     public function subscribe (\Illuminate\Events\Dispatcher $events) : void {
         $class = 'Nasqueron\Notifications\Listeners\AMQPEventListener';
-        $events->listen(
-            'Nasqueron\Notifications\Events\GitHubPayloadEvent',
-            "$class@onGitHubPayload"
-        );
         $events->listen(
             'Nasqueron\Notifications\Events\NotificationEvent',
             "$class@onNotification"
