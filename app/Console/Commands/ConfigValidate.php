b2392d6bafba9e79c376d87a3f52e2bdac916cbf
Sébastien Santoro <dereckson@espace-win.org>
2017-04-09 14:31:30 +0000

    Add command to validate configuration files
    Summary:
    Currently, the command validates JSON syntax,
    using the own PHP parser instead to rely on jsonlint,
    to avoid to depends of Node.JS for one command.
    
    Fixes T1177.
    
    Test Plan:
    New tests check command output is expected when:
    
      - all the files are valid
      - there is a general syntax error
      - there is a specific syntax error
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Maniphest Tasks: T1177
    
    Differential Revision: https://devcentral.nasqueron.org/D949

---
+++ app/Console/Commands/ConfigValidate.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Nasqueron\Notifications\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\FilesystemAdapter;
+
+use App;
+
+class ConfigValidate extends Command {
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'config:validate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Validates JSON configuration files';
+
+    private function getFS () : FilesystemAdapter {
+        return App::make('filesystem')->disk('local');
+    }
+
+    private function getConfigFiles () : array {
+        return array_filter(
+            $this->getFS()->allFiles(),
+
+            // Filters *.json
+            function ($file) : bool {
+                return substr($file, -5) === ".json";
+            }
+        );
+    }
+
+    /**
+     * Executes the console command.
+     */
+    public function handle() : void {
+        $files = $this->getConfigFiles();
+
+        foreach ($files as $file) {
+            $content = $this->getFS()->get($file);
+            if (json_decode($content) === null) {
+                $this->line("$file — " . json_last_error_msg());
+            }
+        }
+    }
+}
---
+++ tests/Console/Commands/ConfigValidateTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Nasqueron\Notifications\Tests\Console\Commands;
+
+use Storage;
+
+class ConfigValidateTest extends TestCase {
+
+    /**
+     * @var string
+     */
+    protected $class = 'Nasqueron\Notifications\Console\Commands\ConfigValidate';
+
+    const TEST_FILE = 'bug.json';
+
+    public function testRegularExecute () {
+        $this->tester->execute(['command' => $this->command->getName()]);
+
+        // When all files are valid, nothing is displayed
+        $this->assertEquals('', $this->tester->getDisplay());
+    }
+
+    /**
+     * @dataProvider provideErrors
+     */
+    public function testSyntaxErrorExecute (string $content, string $error) {
+        $this->populateTestFile($content); // Not JSON
+
+        $this->tester->execute(['command' => $this->command->getName()]);
+
+        // When all files are valid, nothing is displayed
+        $this->assertRegexp("/$error/", $this->tester->getDisplay());
+    }
+
+    /**
+     * Provides invalid JSON strings and associated error
+     */
+    public function provideErrors () : array {
+        return [
+            ["lorem ipsum dolor", "Syntax error"],
+            ['{"}', "Control character error"]
+        ];
+    }
+
+    private function populateTestFile (string $content) : void {
+        Storage::disk('local')->put(self::TEST_FILE, $content);
+    }
+
+    private function deleteTestFile () : void {
+        $fs = Storage::disk('local');
+        if ($fs->exists(self::TEST_FILE)) {
+            $fs->delete(self::TEST_FILE);
+        }
+    }
+
+    public function tearDown () {
+        $this->deleteTestFile();
+        parent::tearDown();
+    }
+}
